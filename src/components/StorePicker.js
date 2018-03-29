155eaff2c7cfa138d38861c9993837461646517a
bwielk <bwielk@gmail.com>
2018-03-29 20:33:12 +0200

    Created first component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render(){
+		return (
+			<form>
+			<p>Hello!</p>
+			<p>How are you?</p>
+			</form>
+		);
+	}
+}
+
+export default StorePicker;
