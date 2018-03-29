6cb4fb781ded7bacb2d60082c49e2e121983e449
bwielk <bwielk@gmail.com>
2018-03-30 00:49:35 +0200

    Routes work

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+	return(
+		<p>"Sorry! The page hasn't been found"</p>
+	)
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './css/style.css'; 
 import StorePicker from './components/StorePicker';
 import App from './components/App';
-import './css/style.css'; 
+import NotFound from './components/NotFound';
 
-render(<App />, document.getElementById('main'));
+
+const Root = () => {
+	return(
+		<BrowserRouter>
+			<div>
+			<Route path= "/" component={StorePicker} />
+			<Route path = "/store/:storeId" component={App} />
+			<Route component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.getElementById('main'));
