2e7893156bee46a9f42779b5c4c237404afc30ee
bwielk <bwielk@gmail.com>
2018-03-29 20:41:01 +0200

    Added styling to the component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,10 @@ import React from 'react';
 class StorePicker extends React.Component {
 	render(){
 		return (
-			<form>
-			<p>Hello!</p>
-			<p>How are you?</p>
+			<form className = "store-selector">
+				<h2>Please Enter The Store</h2>
+				<input type = "text" required placeholder = "Store Name" />
+				<button type = "submit">Visit the store</button>
 			</form>
 		);
 	}
