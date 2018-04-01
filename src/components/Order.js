9bc829e461503d51f5e462e43e853ec9d683a498
bwielk <bwielk@gmail.com>
2018-04-01 15:30:53 +0200

    Order state gets updated

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Order extends React.Component {
+
+	static propTypes = {
+		fishes: PropTypes.object,
+		orders: PropTypes.object
+	}
+
 	render(){
-		return (
-			<p> Order </p>
+		const orderIds = Object.keys(this.props.orders);
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const count = this.props.orders[key];
+			const isAvailable = fish && fish.status === 'available';
+			if(isAvailable){
+				return prevTotal + (count*fish.price || 0);
+			}
+			return prevTotal;
+		}, 0);
+
+	return (
+		<div className = "order-wrap">
+		<h2>Your order</h2>
+		<p>{orderIds}</p>
+		{total}
+		</div>
 		)
 	}
 }
