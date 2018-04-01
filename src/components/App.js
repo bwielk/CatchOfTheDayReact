164aec746535096bb3f10dc848b66339733e9a10
bwielk <bwielk@gmail.com>
2018-04-01 10:57:03 +0200

    Sample fishes are added to the App state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component{
 	constructor(){
@@ -11,6 +12,7 @@ class App extends React.Component{
 			order: {}
 		}
 		this.addFish = this.addFish.bind(this);
+		this.loadSampleFishes = this.loadSampleFishes.bind(this);
 	}
 
 	addFish(fish){
@@ -20,6 +22,12 @@ class App extends React.Component{
 		this.setState({fishes: fishes});
 	}
 
+	loadSampleFishes(){
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render(){
 		return(
 		<div className = "catch-of-the-day">
@@ -27,8 +35,8 @@ class App extends React.Component{
 				<Header tagline = "Fresh Seafood Everyday in Your Local Market" />
 			</div>
 			<Order />
-			<Inventory addFish = {this.addFish}/>
-		</div>
+			<Inventory addFish = {this.addFish} loadSampleFishes = {this.loadSampleFishes}/>
+ 		</div>
 		)
 	}
 }
